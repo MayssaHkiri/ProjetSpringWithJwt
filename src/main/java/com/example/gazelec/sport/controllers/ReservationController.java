a5013a8d9acc21fb29826b7e0d39b8d4dc4339a2
MayssaHkiri <may26saa@gmail.com>
2023-05-09 00:49:34 +0200

    consultation et suppression d'une reservation

--- src/main/java/com/example/gazelec/sport/controllers/ReservationController.java
+++ src/main/java/com/example/gazelec/sport/controllers/ReservationController.java
@@ -1,10 +1,20 @@
 package com.example.gazelec.sport.controllers;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,4 +67,83 @@ public class ReservationController {
 		   return ResponseEntity.ok(new MessageResponse("Reservation registered successfully!"));
 		 
 }
+	
+	
+	/*@GetMapping("/hoiraires")
+	public ResponseEntity<List<Reservation>> getReservationsByDate(@RequestParam("date") Date date) {
+	    Optional<List<Reservation>> reservations = reservationRepo.findByDate(date);
+	    if (reservations.isPresent()) {
+	        List<Reservation> reservationsList = reservations.get();
+	        return ResponseEntity.ok(reservationsList);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}*/
+	/*@GetMapping("/hoiraires")
+	public  List<String>  getReservationsByDate(@RequestParam("date") Date date) {
+		  Optional<List<Reservation>> reservations = reservationRepo.findByDate(date);
+	        if (reservations.isPresent()) {
+	            List<Reservation> reservationsList = reservations.get();
+	            return reservationsList.stream().map(reservation -> reservation.getHdebut() + " - " + reservation.getHfin()).collect(Collectors.toList());
+	        } else {
+	            return Collections.emptyList();
+	        }
+	}*/
+	/*@GetMapping("/horaires")
+	public List<String[]> getReservationsByDate(@RequestParam("date") Date date) {
+	    Optional<List<Reservation>> reservations = reservationRepo.findByDate(date);
+	    if (reservations.isPresent()) {
+	        List<Reservation> reservationsList = reservations.get();
+	        List<String[]> horaires = new ArrayList<>();
+	        for (Reservation reservation : reservationsList) {
+	            horaires.add(new String[] { reservation.getHdebut(), reservation.getHfin() });
+	        }
+	        return horaires;
+	    } else {
+	        return Collections.emptyList();
+	    }
+	}*/
+	@GetMapping("/horaires/{dateString}")
+	public List<String[]> getReservationsByDate(@PathVariable String dateString ) {
+		
+		
+	
+		try {
+		    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		    Date date = new Date(sdf.parse(dateString).getTime());
+		    Optional<List<Reservation>> reservations = reservationRepo.findByDate(date);
+		    if (reservations.isPresent()) {
+		        List<Reservation> reservationsList = reservations.get();
+		        List<String[]> horaires = new ArrayList<>();
+		        for (Reservation reservation : reservationsList) {
+		            horaires.add(new String[] { reservation.getHdebut(), reservation.getHfin() });
+		        }
+		        return horaires;
+		    } else {
+		        return Collections.emptyList();
+		    }
+		} catch (ParseException e) {
+		    // Gérer l'erreur de la conversion de la chaîne de caractères en objet Date
+		    return Collections.emptyList();
+		}
+ 
+	}
+
+	 @GetMapping("/consultation/{userId}")
+	    public ResponseEntity<List<Reservation>> getAllReservationsByUserId(@PathVariable("userId") Long userId) {
+	        Optional<List<Reservation>> optionalReservations = reservationRepo.findAllByUserId(userId);
+	        if (optionalReservations.isPresent()) {
+	            List<Reservation> reservations = optionalReservations.get();
+	            return ResponseEntity.ok().body(reservations);
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+	 @DeleteMapping ("/{id}")
+	 public void deleteReservation (@PathVariable Long id ) {
+		 reservationRepo.deleteById(id) ; 
+		 
+		 }
+	
+	
 }
--- src/main/java/com/example/gazelec/sport/respositories/ReservationRepository.java
+++ src/main/java/com/example/gazelec/sport/respositories/ReservationRepository.java
@@ -1,10 +1,20 @@
 package com.example.gazelec.sport.respositories;
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+import java.lang.Object ; 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.gazelec.sport.models.Reservation;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>{
+	
+	Optional<List<Reservation>> findByDate(Date date);
+	Optional<List<Reservation>> findAllByUserId(Long userId);
 
 }
